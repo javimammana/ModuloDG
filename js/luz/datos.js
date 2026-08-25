446facc9f6c7afb8611ba2324a8fc7cbe2fb25c0
javimammana <javiermammana.dev@gmail.com>
2026-08-25 10:10:19 -0300

    Tarjetas Luz

---
+++ js/luz/datos.js
@@ -0,0 +1,12 @@
+const nombre = "Valentina";
+const fecha = new Date("11/06/2026 21:30");
+const cumple = new Date("11/06/2026");
+const horaEvento = "21:30Hs";
+const alias = "PRESENTE.15.VALEN";
+const aliasTarjetasCvu = "TARJETA.15.VALENTINA"
+const ttCuenta = "Diego Manuel Gatica";
+const cvu = "4530000800013388509100";
+const direccionLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d555.463771553163!2d-64.20771936604757!3d-31.363609282439125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329910042d118b%3A0x87ad3dce433dd3ee!2sEulalia%20Eventos!5e0!3m2!1ses-419!2sar!4v1787661529655!5m2!1ses-419!2sar";
+const coordenadas = "Eulalia Eventos";
+const formCanciones = "https://forms.gle/r8HKYEzRpzbinRk88";
+const fechaGoogle = "https://calendar.app.google/iJuSvoi9M3pCmRTw9";
---
+++ js/luz/mainFamiliares.js
@@ -0,0 +1,30 @@
+const formAsistencia = "https://forms.gle/XfVKqUnnrnDDh11M8";
+    dressCode.innerHTML = "Código de Vestimenta";
+
+
+const asistenciaFecha = "01/09/2026";
+
+asistenciaHasta.innerHTML = `Confirmar asistencia hasta ${asistenciaFecha}`;
+
+const costo = 85000;
+
+valorTarjeta.innerHTML = `Valor de Tarjeta $${costo}.-`;
+
+document.getElementById('copyBtnT').addEventListener('click', function() {
+    const aliasTxtCopy = aliasTarjetasCvu;
+    navigator.clipboard.writeText(aliasTxtCopy)
+    .then(() => {
+        Swal.fire({
+        position: "top",
+        title: "Alias Copiado!",
+        color: "#023859",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        });
+    })
+    .catch(err => {
+        console.log("Error al copiar Alias:", err);
+    });
+});
+
