d3e9491f8a980004f6cbf818deec5e04b69a9312
javimammana <javiermammana.dev@gmail.com>
2026-09-05 17:53:08 -0300

    correccion asistencia valen

--- js/luz/mainFamiliares.js
+++ js/luz/mainFamiliares.js
@@ -2,7 +2,7 @@ const formAsistencia = "https://forms.gle/XfVKqUnnrnDDh11M8";
     dressCode.innerHTML = "Código de Vestimenta";
 
 
-const asistenciaFecha = "01/09/2026";
+const asistenciaFecha = "10/10/2026";
 
 asistenciaHasta.innerHTML = `Confirmar asistencia hasta ${asistenciaFecha}`;
 
